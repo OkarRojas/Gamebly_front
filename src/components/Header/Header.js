89e247c67b13f1d34b318476eba7476e2b86215b
OkarRojas <orojasb@unal.edu.co>
2025-11-13 15:36:16 -0500

    v2 page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,12 +9,16 @@ function Header() {
           <h3>Gamebly</h3>
         </li>
         <li>
+          <form action="/buscar" method="GET" className="barra-busqueda-sencilla">
+            <input type="text" 
+                  name="q" 
+                  placeholder="Buscar..." 
+                  aria-label="Campo de búsqueda" />
+            <button type="submit">Buscar</button>
+        </form>
+        </li>
+        <li>
           <ul className='menu-home-ul'>
-            <li>
-
-              Home
-
-            </li>
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
--- src/components/Naw/Naw.js
+++ src/components/Naw/Naw.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Naw.css';
+import '../config.css';
+import { foto_perfil, user_name } from '../config';
 
 function Naw () {
     return (
@@ -9,16 +11,16 @@ function Naw () {
                     {/* Imagen directa del contenido de Pinterest */}
                     <h5>Welcome Back</h5>
                     <img
-                        src="https://i.pinimg.com/736x/1f/df/9d/1fdf9d1bbd4a0682f5c8ac8c96628286.jpg"
+                        src={foto_perfil}
                         alt="Logo Gamebly"
                         className="Naw-image"
                     />
-                    <h4>Okar Rojas</h4>
+                    <h4>{user_name}</h4>
                 </div>
                 <ul>
                     <li>your library</li>
                     <li>New Games</li>
-                    <li>Home</li>
+                    <li>Recomendaciones</li>
                     <li>etc...</li>
                 </ul>
             </nav>
